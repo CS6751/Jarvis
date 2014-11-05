080fa24db843b0e718d27bdb96303bcc8ae66f3e
namdw <dwn28@cornell.edu>
2014-11-05 01:25:45 -0500

    .

--- human_intent/scripts/prediction.py
+++ human_intent/scripts/prediction.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 # make prediction of human intent depending on the motor data
 
+import rospy
+
+def callback(data)
+    rospy.loginfo(rospy.get_caller_id()+data.data)
 
 def prediction():
     print '1'
+    rospy.init_node('prediction', anonymous=True)
+
+    rospy.Subscriber("/joint_states", sensor_msgs/JointState, callback)
+
+    rospy.spin()
 
 
 if __name__ == '__main__':
