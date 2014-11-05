170303df4d8786d60e4fa66ab1bbe192220c57af
namdw <dwn28@cornell.edu>
2014-11-05 01:08:25 -0500

    stub for prediction code

---
+++ human_intent/scripts/prediction.py
@@ -0,0 +1,14 @@
+# make prediction of human intent depending on the motor data
+
+
+def prediction():
+    print '1'
+
+
+if __name__ == '__main__':
+    try:
+        prediction()
+    except rospy.ROSInterruptException :pass
+
+
+
