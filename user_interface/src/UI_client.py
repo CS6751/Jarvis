f8c3b214ae3cdc03eb9f9e757bd1f78513076345
jadecastro <jdc1177@gmail.com>
2014-11-19 12:11:04 -0500

    moving command publishing functions into the UI_client node

--- user_interface/src/UI_client.py
+++ user_interface/src/UI_client.py
@@ -7,34 +7,83 @@ import geometry_msgs
 from jarvis_perception.srv import *
 import jarvis_perception.msg
 import updateWeights
+import roslib; roslib.load_manifest('pocketsphinx')
+from std_msgs.msg import String
+from actionlib_msgs.msg import GoalID
 
-def return_grips_client():
-    rospy.wait_for_service('return_grips')
-    try:
-        grip_server = rospy.ServiceProxy('return_grips',ReturnGrips)
-        grips = grip_server()
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
+class UI_client:
 
-    #now, update the weights and publish the result
-    try:
-        pub = rospy.Publisher('chatter', jarvis_perception.msg.GraspArray, queue_size=10)
-        rospy.init_node('UI_grip_pub', anonymous=True)
-        r = rospy.Rate(10) # 10hz
+    def __init__(self):
+        rospy.on_shutdown(self.cleanup)
+        rospy.wait_for_service('return_grips')
+        self.msg = GoalID()
+
+        try:
+            grip_server = rospy.ServiceProxy('return_grips',ReturnGrips)
+            grips = grip_server()
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+
+        self.pubgoal = rospy.Publisher('goalID', GoalID)
+        rospy.Subscriber('recognizer/output', String, self.speechCb)
+
+        r = rospy.Rate(10.0)
         while not rospy.is_shutdown():
             gripsWithUpdatedWeights = updateWeights.updateWeights(grips)
             rospy.loginfo(gripsWithUpdatedWeights)
-            pub.publish(gripsWithUpdatedWeights)
-            return grips.grips
+            rospy.loginfo(self.msg)
+            self.pubgoal.publish(self.msg)
             r.sleep()
-    except rospy.ROSInterruptException: pass 
+            # testing
 
-def usage():
-    return "requests grip positions/weights from the perception module"
+    def speechCb(self, msg):
+        rospy.loginfo(msg.data)
+        rospy.loginfo(msg)
+
+        if msg.data.find("come here") > -1:
+            self.msg.id = 1
+        if msg.data.find("move back") > -1:
+            self.msg.id = 2
+
+        rospy.loginfo(self.msg)
+        self.pubgoal.publish(self.msg)
+
+    # def return_grips_client():
+
+    #     try:
+    #         grip_server = rospy.ServiceProxy('return_grips',ReturnGrips)
+    #         grips = grip_server()
+    #     except rospy.ServiceException, e:
+    #         print "Service call failed: %s"%e
+
+    #     #now, update the weights and publish the result
+    #     try:
+    #         pub = rospy.Publisher('return_grips', jarvis_perception.msg.GraspArray, queue_size=10)
+    #         r = rospy.Rate(10) # 10hz
+    #         while not rospy.is_shutdown():
+    #             command = 3
+    #             rospy.loginfo(command)
+    #             pub.publish(rospy.get_time(),command)
+
+    #             gripsWithUpdatedWeights = updateWeights.updateWeights(grips)
+    #             rospy.loginfo(gripsWithUpdatedWeights)
+    #             pub.publish(gripsWithUpdatedWeights)
+    #             print "Publishing..."
+    #             r.sleep()
+    #     except rospy.ROSInterruptException: pass 
+
+    def usage(self):
+        return "requests grip positions/weights from the perception module"
+
+    def cleanup(self):
+        # stop the robot!
+        command = GoalID()
+        rospy.loginfo(command)
+        self.pubgoal.publish(command)
 
 if __name__ == "__main__":
-    grips = return_grips_client()
-    print "Requesting grips"
-    print type(grips)
-    print type(grips.grasps[1])
-    print "grip2 quaternion = " +str(grips.grasps[1].orientation)
+    rospy.init_node('UI_client')
+    try:
+        UI_client()
+    except:
+        pass
