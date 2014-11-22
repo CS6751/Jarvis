51265515da7afc8316f305718536cfe6a4be8de5
namdw <dwn28@cornell.edu>
2014-11-22 14:43:23 -0500

    updated

--- human_intent/scripts/human_intent.py
+++ human_intent/scripts/human_intent.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
+from Intent.msg import intent
 
 def human_intent():
-    pub = rospy.Publisher('intents', String, queue_size=10)
+    pub = rospy.Publisher('intents', intent, queue_size=10)
     rospy.init_node('human_intent', anonymous=True)
     r = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        inte = String()
-     	inte = "sdafsada"
+        inte = intent()
+     	# inte = "sdafsada"
         pub.publish(inte)
         r.sleep()
         
