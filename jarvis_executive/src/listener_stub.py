8e3dfc710fc756b88185ab3a49d66f86768e6c35
Elijah Lee <elijah4rain@gmail.com>
2014-12-03 01:11:45 -0500

    Update listener_stub.py

--- jarvis_executive/src/listener_stub.py
+++ jarvis_executive/src/listener_stub.py
@@ -5,6 +5,7 @@ import rospy
 from std_msgs.msg import String
 from actionlib_msgs.msg import GoalID
 from human_intent.msg import Intent
+from jarvis_planner.msg import PlanStatus
 
 
 def user_interface_callback(userdata):
@@ -18,12 +19,20 @@ def human_intent_callback(userdata):
         print 'intent Hold!'
     else:
 	    print 'intent Adjust!'
+
+def jarvis_planner_callback(userdata):
+    if userdata.PlanStatus == True:
+    	print 'plan ready!'
+    else:
+    	print 'wait for planning...'
+
 def listener():
 
     rospy.init_node('listener_stub', anonymous=True)
 
     rospy.Subscriber('robot_cmd_trial', GoalID, user_interface_callback)
-    rospy.Subscriber('intents_trial',Intent, human_intent_callback)
+    rospy.Subscriber('intents_trial', Intent, human_intent_callback)
+    rospy.Subscriber('PlanStatus_trial', PlanStatus, jarvis_planner_callback)
     
     rospy.spin()
         
