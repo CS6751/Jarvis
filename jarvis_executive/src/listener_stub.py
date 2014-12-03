dbbd85c0acd03589b4f62265ee89074654e9b9b7
Elijah Lee <elijah4rain@gmail.com>
2014-12-03 18:23:16 -0500

    Update listener_stub.py

--- jarvis_executive/src/listener_stub.py
+++ jarvis_executive/src/listener_stub.py
@@ -53,7 +53,7 @@ class Foo(smach.State):
             rospy.Subscriber('intents_trial', Intent, human_intent_callback)
             rospy.Subscriber('PlanStatus_trial', PlanStatus, jarvis_planner_callback)
     	
-    	    a = (UI_COUNTER==1) and (HI_COUNTER==1) and (JP_COUNTER==1):
+    	    a = (UI_COUNTER==1) and (HI_COUNTER==1) and (JP_COUNTER==1)
     	print 'gonna shift to outcome4!!!!!!'
     	return 'outcome1' 
     
