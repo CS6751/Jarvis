e6514d3a6555aed1b28b588331762b56ff70c18b
Ben <bzreinhardt@gmail.com>
2014-12-07 18:46:21 -0500

    working box around hand filter

--- jarvis_pointcloud/src/example.cpp
+++ jarvis_pointcloud/src/example.cpp
@@ -1,108 +1,111 @@
-#include <ros/ros.h>
-// PCL specific includes
-#include <sensor_msgs/PointCloud2.h>
-#include <geometry_msgs/PointStamped.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <tf/transform_listener.h>
+#include "ros/ros.h"
+#include "tf/transform_listener.h"
+#include "tf/message_filter.h"
+#include "message_filters/subscriber.h"
+#include "sensor_msgs/PointCloud2.h"
+#include <pcl_ros/point_cloud.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/filters/passthrough.h>
-#include <Eigen/Geometry>
-//
-ros::Publisher pub;
-geometry_msgs::PointStamped center;
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_types.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+#include <pcl_ros/transforms.h>
 
-// gets a spherical cloud centered on the center point 
-/*
-void getSphereCloud(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr &result )
-
+class CloudDrawer
 {
-    pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud;
-    pcl::PassThrough<pcl::PointXYZ> xfilter;
-    xfilter.setInputCloud(cloud);
-    xfilter.setFilterFieldName("x");
-    xfilter.setFilterLimits(-0.1, 0.1);
-    xfilter.filter(*temp_cloud);
-    
-    pcl::PassThrough<pcl::PointXYZ> yfilter;
-    yfilter.setInputCloud(temp_cloud);
-    yfilter.setFilterFieldName("y");
-    yfilter.setFilterLimits(-0.1, 0.1);
-    yfilter.filter(*temp_cloud);
-
-    pcl::PassThrough<pcl::PointXYZ> zfilter;
-    zfilter.setInputCloud(temp_cloud);
-    zfilter.setFilterFieldName("z");
-    zfilter.setFilterLimits(-0.1, 0.1);
-    zfilter.filter(result);
-
-
-}
-*/
-//
- 
-    void 
-cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
-{
-    tf::TransformListener listener;
-    // Create a container for the data.
-    sensor_msgs::PointCloud2 output;
-    // Create a pointcloud to do processing
-    pcl::PointCloud<pcl::PointXYZ>::Ptr input_pcl;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr output_pcl;
-    // Find the center point in the point cloud frame
-    sensor_msgs::PointCloud2 world_cloud;
-    std::string world_frame = input->header.frame_id;
-    tf::StampedTransform world_tf;
-    try{
-	//transform the point to world coordinates
-	listener.lookupTransform("/target",world_frame, ros::Time(0),world_tf);	
-    // Convert PC2 to pointCloud
-	pcl::fromROSMsg(*input, *input_pcl);
-    //
-    //DO Data processing here ...
-	output_pcl = input_pcl;
-//Convert back to pointcloud
-	pcl::toROSMsg(*output_pcl, output);
-	// Publish the data.
-
-	pub.publish (output);
-
-
-
-    }
-    catch (tf::TransformException ex){
-	ROS_ERROR("%s",ex.what());
-	ros::Duration(1.0).sleep();
-    }
-}
-
-    int
-main (int argc, char** argv)
-{
-    
-    /*
-    std::string topic;
-    if (argc > 1)
+    public:
+	CloudDrawer() : tf_(),  target_frame_("target")
     {
-	topic = argv[0];
-    }
-    else
-    {
-	topic = "/camera/depth_registered/points";
-    }
-    */
-    // Initialize ROS
-    ros::init (argc, argv, "my_pcl_tutorial");
-    ros::NodeHandle nh;
-    ROS_INFO("Started looking at Pointcloud");
-    // Create a ROS subscriber for the input point cloud
-    std::string topic =  "/camera/depth_registered/points";
-     ros::Subscriber sub = nh.subscribe (topic, 1, cloud_cb);
-    // Create a ROS publisher for the output point cloud
-    pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
+	point_sub_.subscribe(n_, "/camera/depth/points", 10);
+	tf_filter_ = new tf::MessageFilter<sensor_msgs::PointCloud2>(point_sub_, tf_, target_frame_, 10);
+	tf_filter_->registerCallback( boost::bind(&CloudDrawer::cloud_cb, this, _1) );
+	pub_ = n_.advertise<sensor_msgs::PointCloud2> ("output" , 1);
+    } ;
 
-    // Spin
-    ros::spin ();
-}
+    private:
+	message_filters::Subscriber<sensor_msgs::PointCloud2> point_sub_;
+	tf::TransformListener tf_;
+	tf::MessageFilter<sensor_msgs::PointCloud2> * tf_filter_;
+	ros::NodeHandle n_;
+	std::string target_frame_;
+	ros::Publisher pub_;
+	tf::StampedTransform tf_world_;
+	pcl::PCLPointCloud2 pcl_pc2_;
+	//pcl::PointCloud<pcl::PointXYZ>::Ptr temp_ptr_;
+	//pcl::PointCloud<pcl::PointXYZ>::Ptr temp_ptr2_;
+
+	void filterPassThrough (const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud,  
+	pcl::PointCloud<pcl::PointXYZ>	&result)
+	{
+	//    FPS_CALC_BEGIN;
+	    pcl::PassThrough<pcl::PointXYZ> pass;
+	    pass.setFilterFieldName ("z");
+	    pass.setFilterLimits (-0.1, 0.1);
+	    pass.setKeepOrganized (false);
+	    pass.setInputCloud (cloud);
+	    pass.filter (result);
+	  //  FPS_CALC_END("filterPassThrough");
+	}
+
+	//  Callback to register with tf::MessageFilter to be called when transforms are available
+	void cloud_cb(const boost::shared_ptr<const sensor_msgs::PointCloud2>& input) 
+	{
+	    sensor_msgs::PointCloud2 output;
+	    std::string world_frame = "/camera_link";
+	    tf::Vector3 origin;
+	    try 
+	    {
+
+		tf_.lookupTransform(world_frame,"/target",ros::Time(0),tf_world_);
+		pcl_ros::transformPointCloud("/target",*input,output,tf_);
+		origin = tf_world_.getOrigin();
+//		std::cout << output.header.frame_id << std::endl;
+	    }
+	    catch (tf::TransformException &ex) 
+	    {
+		printf ("Failure %s\n", ex.what()); //Print exception which was caught
+	    }
+	    
+	    pcl::PCLPointCloud2 pcl_pc2;
+	    pcl_conversions::toPCL(output,pcl_pc2);
+	    pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+	    pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
+
+	    ///////////////////////////////////////////////////////////////
+	    //  /*
+	    //     * Pass through Filtering
+	    //        */
+	    //          ////////////////////////////////////////////////////
+	    pcl::PointCloud<pcl::PointXYZ>::Ptr pass_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+	    pcl::PassThrough<pcl::PointXYZ> pass;
+	    pass.setInputCloud(temp_cloud);
+	    pass.setFilterFieldName("z");
+	    pass.setFilterLimits(-0.1,0.1);
+	    pass.filter(*pass_cloud);
+	    pass.setInputCloud(pass_cloud);
+	    pass.setFilterFieldName("y");
+	    pass.setFilterLimits(-0.1,0.1);
+	    pass.filter(*pass_cloud);
+      	    pass.setInputCloud(pass_cloud);
+	    pass.setFilterFieldName("z");
+	    pass.setFilterLimits(-0.1,0.1);
+	    pass.filter(*pass_cloud);
+	    /////
+	    // Turn pass_cloud into output //
+	    // ///
+	  pcl::toROSMsg(*pass_cloud,output);
+		
+		pub_.publish(output);
+
+	     };
+
+};
+	
+	
+	                                                                                                   int main(int argc, char ** argv)
+	                                                                                                   {
+	                                                                                                     ros::init(argc, argv, "pose_drawer"); //Init ROS
+        CloudDrawer pd; //Construct class
+	                                                                                                         ros::spin(); // Run until interupted 
+	                                                                                                         };
