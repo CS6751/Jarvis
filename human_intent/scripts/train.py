1105485ecae218eb588a35ee6d253825d3cabf67
namdw <dwn28@cornell.edu>
2014-12-08 20:45:54 -0500

    training and classifying

--- human_intent/scripts/train.py
+++ human_intent/scripts/train.py
@@ -6,52 +6,83 @@ from time import time
 #READ DATA FROM DATA FILES
 X = []
 Y = []
+
 file = open('traindata.txt')
 for line in file:
-	data = [float(d) for d in line.split(',')]
+	data = []
+	for d in line.split(','):
+		if d != '\n':
+			data.append(float(d))
+	# data = [float(d) for d in line.split(',')]
 	X.append(data)
 file.close()
 
 file = open('trainout.txt')
 for line in file:
-	data = float(line)
-	Y.append([data])
+	data = []
+	for d in line.split(','):
+		if d != '\n':
+			data.append(float(d))
+	Y.append(data)
 file.close()
 
+correct = 0
+for i in range(len(Y)):
+	trainX = []
+	trainY = []
+	testX = []
+	testY = []
+	for j in range(len(Y)):
+		if j==i:
+			testX.append(X[j])
+			testY.append(Y[i])
+		else:
+			trainX.append(X[j])
+			trainY.append(Y[j])
 
-#CONVERT DATA TO NUMPY ARRAY
-X, Y = np.array(X), np.array(Y)
+	# print trainX
+	# print trainY
+	# print testX
+	# print testY
+	#CONVERT DATA TO NUMPY ARRAY
+	trainX, trainY = np.array(trainX), np.array(trainY)
+	testX = np.array(testX)
 
-#CREATE NETWORK
-nnet = cb.Network()
+	#CREATE NETWORK
+	nnet = cb.Network()
 
-#CREATE LAYERS
-Lin = cb.Layer( 2, names= ['a','b'] )
-Lhidden = cb.Layer( 2, cb.LogisticNeuron , names= ['c','d'] )
-Lout = cb.Layer( 1, cb.LogisticNeuron , names= ['e'] )
-bias = cb.Layer( 1, cb.BiasUnit, names= ['bias'] )
+	#CREATE LAYERS
+	Lin = cb.Layer(12)
+	Lhidden = cb.Layer( 5, cb.LogisticNeuron)
+	Lout = cb.Layer( 1, cb.LogisticNeuron)
+	bias = cb.Layer( 1, cb.BiasUnit)
 
-#ADD LAYERS TO NETWORK
-nnet.addInputLayer(Lin)
-nnet.addLayer(Lhidden)
-nnet.addOutputLayer(Lout)
-nnet.addAutoInputLayer(bias)
+	#ADD LAYERS TO NETWORK
+	nnet.addInputLayer(Lin)
+	nnet.addLayer(Lhidden)
+	nnet.addOutputLayer(Lout)
+	nnet.addAutoInputLayer(bias)
 
-#CONNECT LAYERS
-Lin.connectTo(Lhidden)
-Lhidden.connectTo(Lout)
-bias.connectTo(Lhidden)
-bias.connectTo(Lout)
+	#CONNECT LAYERS
+	Lin.connectTo(Lhidden)
+	Lhidden.connectTo(Lout)
+	bias.connectTo(Lhidden)
+	bias.connectTo(Lout)
 
-#CREATE BATCH TRAINER
-rate = 0.1
-batch = cb.Trainer( nnet, X, Y, rate )
+	#CREATE BATCH TRAINER
+	rate = 0.1
+	batch = cb.Trainer( nnet, trainX, trainY, rate )
 
-#TRAIN
-t1 = time()
-batch.epochs(10000)
-print "Time CyBrain {}".format(time()-t1)
+	#TRAIN
+	t1 = time()
+	batch.epochs(15)
+	print "Time CyBrain {}".format(time()-t1)
 
-#PRINT RESULTS
-for x in X:
-    print "{} ==> {}".format( x, nnet.activateWith(x, return_value= True) )
+	result = nnet.activateWith(testX[0], return_value= True)
+	#PRINT RESULTS
+	# for x in testX:
+	#     print "{} ==> {}".format( x, result )
+	print testY[0]
+	print result[0]
+	predict = result[0]
+
