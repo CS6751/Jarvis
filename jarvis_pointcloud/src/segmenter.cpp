9900e1692352de3296d50aa2dd130c49d9e0e38c
Ben <bzreinhardt@gmail.com>
2014-12-08 19:14:50 -0500

    60% working board id

--- jarvis_pointcloud/src/segmenter.cpp
+++ jarvis_pointcloud/src/segmenter.cpp
@@ -1,8 +1,10 @@
 #include "ros/ros.h"
+#include <ros/console.h>
 #include "tf/transform_listener.h"
 #include "tf/message_filter.h"
 #include "message_filters/subscriber.h"
 #include "sensor_msgs/PointCloud2.h"
+#include <std_msgs/String.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/filters/passthrough.h>
@@ -17,11 +19,16 @@
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
+//Includes for filtering
+#include <pcl/filters/extract_indices.h>
 
 class CloudDrawer
 {
     public:
-	CloudDrawer() : tf_(),  target_frame_("target")
+	CloudDrawer() : tf_(),
+	target_frame_("target"),
+	coefficients_(new pcl::ModelCoefficients),
+	inliers_(new pcl::PointIndices)
     {
 	point_sub_.subscribe(n_, "/camera/depth/points", 10);
 	tf_filter_ = new tf::MessageFilter<sensor_msgs::PointCloud2>(point_sub_, tf_, target_frame_, 10);
@@ -38,6 +45,10 @@ class CloudDrawer
 	ros::Publisher pub_;
 	tf::StampedTransform tf_world_;
 	pcl::PCLPointCloud2 pcl_pc2_;
+	pcl::ModelCoefficients::Ptr coefficients_;
+	pcl::PointIndices::Ptr inliers_;
+	pcl::SACSegmentation<pcl::PointXYZ> seg_;
+	
 	//pcl::PointCloud<pcl::PointXYZ>::Ptr temp_ptr_;
 	//pcl::PointCloud<pcl::PointXYZ>::Ptr temp_ptr2_;
 
@@ -97,21 +108,55 @@ class CloudDrawer
 	    pass.setFilterFieldName("z");
 	    pass.setFilterLimits(-0.1,0.1);
 	    pass.filter(*pass_cloud);
-	    /////
-	    // Turn pass_cloud into output //
-	    // ///
-	  pcl::toROSMsg(*pass_cloud,output);
-		
-		pub_.publish(output);
 
-	     };
+	/////////////////////
+	// Find a plane in the target point cloud 
+	// //////////////////////////
+	    
+	    seg_.setOptimizeCoefficients (true);
+	seg_.setModelType(pcl::SACMODEL_PLANE);
+	seg_.setMethodType(pcl::SAC_RANSAC);
+	seg_.setDistanceThreshold(0.01);
+	seg_.setInputCloud(pass_cloud);
+	seg_.segment (*inliers_, *coefficients_);
+	if (inliers_->indices.size () == 0)
+	{
+	    ROS_INFO ("Could not estimate a planar model for the given dataset.");
+	}
+	std::stringstream out;
+	out << "Model coefficients: " << coefficients_->values[0] << " " 
+	    << coefficients_->values[1] << " "
+	    << coefficients_->values[2] << " " 
+	    << coefficients_->values[3] << std::endl;
+	std_msgs::String msg;
+	msg.data = out.str();
+	ROS_INFO("%s",msg.data.c_str());
+
+	//////////////////////////////////////
+	// Pull out the points in the plane
+	//////////////////////////////////////
+	pcl::ExtractIndices<pcl::PointXYZ> extract;
+	pcl::PointCloud<pcl::PointXYZ>::Ptr filt_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+	extract.setInputCloud(pass_cloud);
+	extract.setIndices(inliers_);
+	extract.setNegative(true);
+	extract.filter(*filt_cloud);
+	/////
+	// Turn pass_cloud into output //
+	// ///
+	pcl::toROSMsg(*filt_cloud,output);
+
+	pub_.publish(output);
+
+	};
 
 };
-	
-	
-	                                                                                                   int main(int argc, char ** argv)
-	                                                                                                   {
-	                                                                                                     ros::init(argc, argv, "pose_drawer"); //Init ROS
-        CloudDrawer pd; //Construct class
-	                                                                                                         ros::spin(); // Run until interupted 
-	                                                                                                         };
+
+
+int main(int argc, char ** argv)
+{
+    ros::init(argc, argv, "pose_drawer"); //Init ROS
+ROS_INFO("segmenter started");
+    CloudDrawer pd; //Construct class
+    ros::spin(); // Run until interupted 
+};
