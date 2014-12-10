694e36c22c724d1215d4623e1a3fb38bbd90837d
Elijah Lee <elijah4rain@gmail.com>
2014-12-10 02:29:58 -0500

    Update test_listening.py

--- jarvis_executive/src/test_listening.py
+++ jarvis_executive/src/test_listening.py
@@ -8,23 +8,26 @@ from human_intent.msg import Intent
 from jarvis_planner.msg import PlanStatus
 from jarvis_executive.msg import *
 
-
-def callback(userdata):
-    if userdata.kill:
-        print 'listening Kill topic!!!'
-        #rospy.loginfo(userdata)
-    	print num
-    	
-def listener():
-    num = 0
+class listener(object):
     
-    while not rospy.is_shutdown():
+    def __init__(self):
+        self.num = 0
+        r = rospy.Rate(10)
         rospy.init_node('test_listening', anonymous=True)
-        #rospy.Subscriber('robot_cmd_trial', GoalID, user_interface_callback)
-        #rospy.Subscriber('intents_trial', Intent, human_intent_callback)
-        #rospy.Subscriber('PlanStatus_trial', PlanStatus, jarvis_planner_callback)
-        rospy.Subscriber('Kill', Kill, callback)
-        num += 1
-
+        
+        while not rospy.is_shutdown():
+            #rospy.Subscriber('robot_cmd_trial', GoalID, user_interface_callback)
+            #rospy.Subscriber('intents_trial', Intent, human_intent_callback)
+            #rospy.Subscriber('PlanStatus_trial', PlanStatus, jarvis_planner_callback)
+            rospy.Subscriber('Kill', Kill, callback)
+            self.num += 1
+            r.sleep()
+            
+    def callback(self, userdata):
+        if userdata.kill:
+            print 'listening Kill topic!!!'
+            #rospy.loginfo(userdata)
+            print self.num
+            
 if __name__ == '__main__':
     listener()
