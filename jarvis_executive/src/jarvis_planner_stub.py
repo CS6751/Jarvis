4c80d8b4625b9136f6c5e3ded705a88dcbdee8dd
Elijah Lee <elijah4rain@gmail.com>
2014-12-10 05:35:45 -0500

    Update jarvis_planner_stub.py

--- jarvis_executive/src/jarvis_planner_stub.py
+++ jarvis_executive/src/jarvis_planner_stub.py
@@ -6,32 +6,34 @@ from std_msgs.msg import String
 from jarvis_planner.msg import PlanStatus
 from jarvis_executive.msg import *
 
-def jarvis_planner_pub():
-    pub = rospy.Publisher('PlanStatus_trial', PlanStatus, queue_size=10)
-    rospy.init_node('jarvis_planner_stub', anonymous=True)
-    r = rospy.Rate(1) # 10hz
+class jarvis_planner_pub(object):
     
-    counter = 0
-    num = 0
-    while not rospy.is_shutdown():
-        rospy.Subscriber('PlanCommand', PlanCommand, callback)
+    def __init__(self):
+        pub = rospy.Publisher('PlanStatus_trial', PlanStatus, queue_size=10)
+        r = rospy.Rate(1) # 1hz
+        self.counter = 0
+        self.num = 0
+    
+        while not rospy.is_shutdown():
+            rospy.Subscriber('PlanCommand', PlanCommand, self.callback)
+            print self.counter
+            self.counter += 1
+            r.sleep()
         
-         
-        print counter
-        counter += 1
-        r.sleep()
-        
-def callback(userdata):
-    if userdata.plancommand:
-        print 'begin planning for basemove'
-        num += 1
-        if num >= 5:
-            pub.publish(True)
-    else:
-        pub.publish(False)
+    def callback(userdata):
+        if userdata.plancommand:
+            print 'begin planning for basemove'
+            self.num += 1
+            print self.num
+            if self.num >= 5:
+                print 'plan ready'
+                pub.publish(True)
+        else:
+            pub.publish(False)
         
 
 if __name__ == '__main__':
+    rospy.init_node('jarvis_planner_stub', anonymous=True)
     try:
         jarvis_planner_pub()
     except rospy.ROSInterruptException: pass
