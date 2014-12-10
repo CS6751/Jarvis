c3aefb2ea6a20d3af4f99e5a4634a941663a6465
Elijah Lee <elijah4rain@gmail.com>
2014-12-10 16:22:43 -0500

    Update jarvis_planner_stub.py

--- jarvis_executive/src/jarvis_planner_stub.py
+++ jarvis_executive/src/jarvis_planner_stub.py
@@ -10,29 +10,27 @@ class jarvis_planner_pub(object):
     
     def __init__(self):
         pub = rospy.Publisher('PlanStatus_trial', PlanStatus, queue_size=10)
-        r = rospy.Rate(1) # 1hz
-        self.counter = 0
+        r = rospy.Rate(10) # 10hz
         self.num = 0
         self.transition = 0
     
         while not rospy.is_shutdown():
             if self.transition == 0:
                 rospy.Subscriber('PlanCommand', PlanCommand, self.callback)
-                print self.counter
-                self.counter += 1
-            
+             
             if self.transition == 1:
                 print 'base plan ready'
+                self.transition = 0
                 pub.publish(True)
-            
+                
             r.sleep()
         
     def callback(self, userdata):
-        if userdata.plancommand:
+        if userdata.plancommand and self.transition == 0:
             print 'begin planning for basemove'
             self.num += 1
             print self.num
-            if self.num >= 5:
+            if self.num >= 80:
                 self.transition = 1
                 
        
