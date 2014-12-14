920b0935b5175f1bf1410679b991c4ef8e183c1b
jadecastro <jdc1177@gmail.com>
2014-12-13 22:04:23 -0500

    robustify to changes in board yaw

--- user_interface/src/updateWeights.py
+++ user_interface/src/updateWeights.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from numpy import *
 from collections import OrderedDict
 
-def updateWeights(grips, utterance, axisAlignedBox, person_rot):
+def updateWeights(grips, utterance, axisAlignedBox, boardRotation):
 
     # get the position and x-y dimensions in the box frame 
     # Assume: 
@@ -21,6 +21,10 @@ def updateWeights(grips, utterance, axisAlignedBox, person_rot):
     ycent = axisAlignedBox.pose.position.z
     position = axisAlignedBox.pose.position # position in camera frame
     orientation = axisAlignedBox.pose.orientation # orientation wrt camera frame (Q: should this come in as a tf?)
+    boardRotationRoll = boardRotation[0]
+    boardRotationPitch = boardRotation[1]
+    boardRotationYaw = boardRotation[2]
+    
     print utterance
     # print xspan, yspan, position, orientation
 
@@ -50,85 +54,92 @@ def updateWeights(grips, utterance, axisAlignedBox, person_rot):
     # Assign a likelihood based on the keyword and the person pose
     likelihood = OrderedDict([('BoxAnywhere',0),('BoxCenter',0),('BoxLeft',0),('BoxRight',0),('BoxBottom',0),('BoxTop',0),('BoxBottomLeft',0),('BoxBottomRight',0),('BoxTopLeft',0),('BoxTopRight',0)])
     angleInPersonFrame = None
+    elevationAngle = None
     if utterance.find("anywhere") > -1:
         likelihood['BoxAnywhere'] = 1
     elif utterance.find("center") > -1:
         likelihood['BoxCenter'] = 1
-    elif utterance.find("bottom left") > -1 or utterance.find("lower left") > -1:
+    elif utterance.find("near left") > -1 or utterance.find("front left") > -1 or utterance.find("bottom left") > -1 or utterance.find("lower left") > -1:
         angleInPersonFrame = -0.75*pi
-    elif utterance.find("bottom right") > -1 or utterance.find("lower right") > -1:
+        elevationAngle = 0.5*pi
+    elif utterance.find("near right") > -1 or utterance.find("front right") > -1 or utterance.find("bottom right") > -1 or utterance.find("lower right") > -1:
         angleInPersonFrame = -0.25*pi
-    elif utterance.find("top left") > -1 or utterance.find("upper left") > -1:
+        elevationAngle = 0.5*pi
+    elif utterance.find("far left") > -1 or utterance.find("rear left") > -1 or utterance.find("back left") > -1 or utterance.find("top left") > -1 or utterance.find("upper left") > -1:
         angleInPersonFrame = 0.75*pi
-    elif utterance.find("top right") > -1 or utterance.find("upper right") > -1:
+        elevationAngle = 0.5*pi
+    elif utterance.find("far right") > -1 or utterance.find("rear right") > -1 or utterance.find("back right") > -1 or utterance.find("top right") > -1 or utterance.find("upper right") > -1:
         angleInPersonFrame = 0.25*pi
+        elevationAngle = 0.5*pi
     elif utterance.find("left") > -1:
         angleInPersonFrame = pi
     elif utterance.find("right") > -1:
         angleInPersonFrame = 1e-5
-    elif utterance.find("bottom") > -1:
+    elif utterance.find("near") > -1 or utterance.find("front") or utterance.find("bottom") > -1 or utterance.find("lower") > -1:
         angleInPersonFrame = -0.5*pi
-    elif utterance.find("top") > -1:
+        elevationAngle = 0.5*pi
+    elif utterance.find("far") > -1 or utterance.find("rear") > -1 or utterance.find("back") > -1 or utterance.find("top") > -1 or utterance.find("upper"):
         angleInPersonFrame = 0.5*pi
+        elevationAngle = 0.5*pi
     else:
+        # rospy.logerr('I thought I heard a location keyword, but I did not understand it.')
         raise RuntimeWarning('I thought I heard a location keyword, but I did not understand it.')
 
     if not angleInPersonFrame == None:
         # Transform from the person frame into the box frame and then compute the likelihoods
-        if not person_rot:
+        if not boardRotationRoll:
             # No person in view: default to using camera frame
-            person_rot = 0
+            boardRotationRoll = 0
 
-        c = cos(angleInPersonFrame+person_rot)
-        s = sin(angleInPersonFrame+person_rot)
-
-        #TODO: normalize sum to one
+        c = cos(angleInPersonFrame+boardRotationRoll)
+        s = sin(angleInPersonFrame+boardRotationRoll)
+        if (boardRotationYaw > 0.5*pi and boardRotationYaw < 0.75*pi) or (boardRotationYaw < -0.5*pi and boardRotationYaw > -0.75*pi):
+            c = -c
+            
+        likelihood['BoxTop']        = max(s, 0)**2
+        likelihood['BoxBottom']     = max(-s, 0)**2
         likelihood['BoxLeft']       = max(-c, 0)**2
         likelihood['BoxTopLeft']    = max(-c, 0)**2 + max(s, 0)**2
-        likelihood['BoxTop']        = max(s, 0)**2
         likelihood['BoxTopRight']   = max(c, 0)**2 + max(s, 0)**2
         likelihood['BoxRight']      = max(c, 0)**2
         likelihood['BoxBottomRight']= max(c, 0)**2 + max(-s, 0)**2
-        likelihood['BoxBottom']     = max(-s, 0)**2
         likelihood['BoxBottomLeft'] = max(-c, 0)**2 + max(-s, 0)**2
 
         # print likelihood['BoxTopLeft']
         # print likelihood['BoxLeft']
         # print likelihood['BoxBottomLeft']
 
-    y, x = mgrid[slice(ycent-0.5*yspan, ycent+0.5*yspan + 0.001, 0.001),
-                 slice(xcent-0.5*xspan, xcent+0.5*xspan + 0.001, 0.001)]
-    posArray = empty(x.shape + (2,))
-    posArray[:,:,0] = x; posArray[:,:,1] = y
-    z = 0
-    for j, name in enumerate(likelihood):
-        z += likelihood[name] * var[name].pdf(posArray)
-    zMax = z.max() # normalizing the distribution - TODO: this is only an estimate-- fix
-    # print zMax
-
-    for i in range(len(oldGrips)):
-        xgrip = oldGrips[i].point.y
-        ygrip = oldGrips[i].point.z
-        # pos = oldGrips[i].point[0]
-        # print oldGrips[i]
-        pos = 2*[[]]
-        pos[0] = float(xgrip); pos[1] = float(ygrip)
-        zSum = 0
+    if likelihood:
+        y, x = mgrid[slice(ycent-0.5*yspan, ycent+0.5*yspan + 0.001, 0.001),
+                     slice(xcent-0.5*xspan, xcent+0.5*xspan + 0.001, 0.001)]
+        posArray = empty(x.shape + (2,))
+        posArray[:,:,0] = x; posArray[:,:,1] = y
+        z = 0
         for j, name in enumerate(likelihood):
-            # print name
-            # print likelihood[name]
-            # print var[name].pdf(pos)
-            zSum += likelihood[name] * var[name].pdf(pos)
-        # print zSum
+            z += likelihood[name] * var[name].pdf(posArray)
+        zMax = z.max() # normalizing the distribution - TODO: this is only an estimate-- fix
+        # print zMax
 
-        # Update the ith grip's weight.  (posterior) = (grip likelihood) * (prior)
-        weight[i] = zSum/zMax * oldGrips[i].weight
-        grip[i] = GraspBox(oldGrips[i].point, oldGrips[i].orientation, weight[i])
+        for i in range(len(oldGrips)):
+            xgrip = oldGrips[i].point.y
+            ygrip = oldGrips[i].point.z
+            # print oldGrips[i]
+            pos = 2*[[]]
+            pos[0] = float(xgrip); pos[1] = float(ygrip)
+            zSum = 0
+            for j, name in enumerate(likelihood):
+                # print name
+                # print likelihood[name]
+                # print var[name].pdf(pos)
+                zSum += likelihood[name] * var[name].pdf(pos)
+            # print zSum
 
-        newgrips.grasps.append(grip[i])
-        # print grip[i]
+            # Update the ith grip's weight.  (posterior) = (grip likelihood) * (prior)
+            weight[i] = zSum/zMax * oldGrips[i].weight
+            grip[i] = GraspBox(oldGrips[i].point, oldGrips[i].orientation, weight[i])
 
-    # newgrips.header.frame_id = '1'
+            newgrips.grasps.append(grip[i])
+            # print grip[i]
 
     return newgrips, likelihood, var
 
